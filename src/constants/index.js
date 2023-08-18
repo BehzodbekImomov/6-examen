d8fe3fcb3a47d788b379c10c1cccbb30300f03e9
mr <behzodbeki65@gmail.com>
2023-08-18 14:05:02 +0500

    end commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ export const ROLE = "role";
 export const API = "https://blog-backend-production-a0a8.up.railway.app/";
 export const ENDPOINT = API + "api/v1/";
 export const IMG_URL = API + "upload/";
-export const PER_PAGE = 2;
+export const PER_PAGE = 5;
 
 export const limit = 3;
 export const limit_all = 10;
